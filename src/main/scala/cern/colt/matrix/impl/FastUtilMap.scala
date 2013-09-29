6b7ae41546b060289239ce1157328e9fc203fcf3
Daniel Mahler <Daniel.Mahler@bazaarvoice.com>
2013-09-29 01:10:08 -0500

    more cleaning

--- src/main/scala/cern/colt/matrix/impl/FastUtilMap.scala
+++ src/main/scala/cern/colt/matrix/impl/FastUtilMap.scala
@@ -4,30 +4,7 @@ import java.{lang => jl}
 import java.{util => ju}
 import it.unimi.dsi.fastutil.longs._
 
-trait Box[R] {
-  type Raw = R
-  type Wrap
-  type SetType <: ju.Set[Wrap]
-  implicit def box(x: Raw) : Wrap
-  implicit def unbox(x: Wrap) : Raw
-}
-
-abstract class Boxed[R, W](implicit val bx: R => W, val ubx: W => R) extends Box[R]  {
-  //type Raw = R
-  type Wrap = W
-  implicit def box(x: Raw) : Wrap = bx(x)
-  implicit def unbox(x: Wrap) : Raw = ubx(x)
-}
-
-object Box {
-  implicit case object int extends Boxed[Int, jl.Integer]
-  implicit case object long extends Boxed[Long, jl.Long]
-  implicit case object double extends Boxed[Double, jl.Double]
-  implicit case object float extends Boxed[Float, jl.Float]
-  implicit case object ref extends Boxed[AnyRef, jl.Object]
-}
-
-abstract class FastUtilMap[K, V](implicit val bk: Box[K], val bv: Box[V]) {
+abstract class FastUtilMap[K, V]/*(implicit val bk: Box[K], val bv: Box[V]) */{
   type Key = K
   type Value = V
   type BK // = bk.Wrap
